95eaf3017fd77686bc31fa863c2a8ed059a5a7bc
Rupesh More <rupeshmore85@gmail.com>
2012-09-21 09:00:02 -0400

    Commit for Project 1
    2 files for Project P1 ready to be commit

---
+++ p1/Project1.php
@@ -0,0 +1,102 @@
+<html>
+  <head>
+    <meta name="generator" content=
+    "HTML Tidy for Windows (vers 14 February 2006), see www.w3.org">
+    <title>
+      "Rupesh's Project 1 for Web development course"
+    </title>
+    <style type="text/css">
+body
+    {
+    background:url(Bandra-worli-sealink_edit.jpg) fixed;
+    }
+    </style>
+  </head>
+  <body background="Bandra-worli-sealink_edit.jpg" bgproperties="fixed" style=
+  "opacity:0.5;filter:alpha(opacity=50)" onmouseover=
+  "this.style.opacity=1;this.filters.alpha.opacity=100" onmouseout=
+  "this.style.opacity=.9;this.filters.alpha.opacity=80" class="header">
+    <center>
+      <font size="+2"><b><i>"Namaste from Rupesh"</i></b></font>
+    </center>
+    <p align="left">
+      <b><u><font face="Calibri" color="maroon" size="+1">Snippet of My
+      Life:</font></u></b>
+    </p><font face="Georgia" color="black">Hi, I'm Rupesh. I was born in
+    Mumbai, India and 26 years of age. I have done my Bachelors in Electronics
+    Engineering from University of Mumbai in 2007.Since then I have worked as a
+    Software Engineer in an IT Services &amp; Software provider company. I am a
+    backend database developer and have expertise in Oracle technology,
+    Informatica (ETL - Extract, Transform &amp; Load) tool &amp; Webfocus
+    Business Intelligence tool.<br>
+    I have moved to Boston 7 months back in order to provide Level 3 support to
+    an application that we have build for a large Boston based mutual fund
+    &amp; financial services groups.My motive behind registering for Dynamic
+    Web development course is; I want to get more hands on the front end and of
+    course in trying to get the ALM degree from the prestigious Harvard
+    University.</font>
+    <p>
+      <b><u><font face="Calibri">Favorite Quote</font></u> :</b> <font face=
+      "Georgia">What we do in life echoes in eternity!</font><br>
+      <b><u><font face="Calibri">LinkedIn Profile</font></u> :</b> <a href=
+      "http://www.linkedin.com/pub/rupesh-more/14/943/524" target=
+      "_blank"><img src=
+      "http://s4.licdn.com/scds/common/u/img/logos/logo_132x32_2.png" border=
+      "0%" width="60" height="20"></a><br>
+      <b><u><font face="Calibri">Favorite Song</font></u> :</b> <a href=
+      "http://www.youtube.com/watch?v=QGJuMBdaqIw" target="_blank"><img src=
+      "http://lh4.googleusercontent.com/-4QDxins1Vgw/AAAAAAAAAAI/AAAAAAAAGYw/sM3FJY9Asl4/photo.jpg?sz=116"
+      border="0%" width="30" height="20"></a><br>
+    </p>
+    <hr size="-1" noshade color="black">
+    <p>
+      <b><u><font face="Calibri" color="maroon" size="+1">Web
+      Hosting:</font></u></b>
+    </p>
+    <p align="left">
+      <font face="Georgia">I prefer to choose <a href=
+      "http://asmallorange.com/" target="_blank">A Small Orange</a> for web
+      hosting. This is the first time i have registered for web hosting and
+      only time will tell about the experience using Small Orange as a host.
+      Till now faced minor issues but the customer service given is good. I
+      have registered for the Tiny plan and the domain shared is also with
+      Small Orange.</font>
+    </p>
+    <hr size="-1" noshade color="black">
+    <p>
+      <b><u><font face="Calibri" color="maroon" size="+1">Code
+      Editor:</font></u></b>
+    </p>
+    <p align="left">
+      <font face="Georgia">For the past 4 years, I have been using <a href=
+      "http://notepad-plus-plus.org/" target="_blank">Notepad++<img src=
+      "http://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Notepad_plus_plus.png/128px-Notepad_plus_plus.png"
+      border="0%" width="25" height="15"></a> as my text editor. This editor is
+      simple and easy to use for writing/editing the code. It is the best tool
+      which organizes your code into the proper form while highlighting the
+      functions, key words with different colours. It supports many programming
+      languages and i have extensively used it to edit my SQL code. It also
+      supports many plugins which makes the developer's life easy.</font>
+    </p>
+    <hr size="-1" noshade color="black">
+    <p align="left">
+      <b><u><font face="Calibri" color="maroon" size=
+      "+1">Repository:</font></u></b>
+    </p>
+    <p align="left">
+      <font face="Georgia">My GIThub repository : <a href=
+      "https://github.com/rupeshmore85/dwa" target=
+      "_blank">rupeshmore85<img src=
+      "https://a248.e.akamai.net/assets.github.com/images/modules/about_page/octocat.png?1338945074"
+      border="0%" width="25" height="15"></a></font>
+    </p>
+    <div id="footer">
+      <div class="copyright">
+        <center>
+          <b><marquee behavior="alternate">Copyright © Rupesh More, Copyright ©
+          Harvard University, 2012</marquee></b>
+        </center>
+      </div>
+    </div>
+  </body>
+</html>
