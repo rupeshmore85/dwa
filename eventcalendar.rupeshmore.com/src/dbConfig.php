7f99fe8fff640ff7cb5c64924a4e618e511058b5
Rupesh More <rupeshmore85@gmail.com>
2012-12-19 21:09:33 -0500

    Database configuration change
    Database configuration change

--- eventcalendar.rupeshmore.com/src/dbConfig.php
+++ eventcalendar.rupeshmore.com/src/dbConfig.php
@@ -1,6 +1,6 @@
 <?
-	define("EZSQL_DB_USER", "root");			// <-- mysql db user
-	define("EZSQL_DB_PASSWORD", "root");		// <-- mysql db password
+	define("EZSQL_DB_USER", "rupeshmo_admin");			// <-- mysql db user
+	define("EZSQL_DB_PASSWORD", "Password_test1@");		// <-- mysql db password
 	define("EZSQL_DB_NAME", "rupeshmo_p2_rupeshmore_com");		// <-- mysql db pname
 	define("EZSQL_DB_HOST", "localhost");	// <-- mysql server host
 
